3bb7630b8b2bc8849fb3205e5d313b3e7d87efd6
ombito <alvinombito09@gmail.com>
2023-07-17 20:31:38 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+const body = document.createTextNode("Hello, World!");
+newHeader.appendChild(body);
